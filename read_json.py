92e10065d87dcdb2460179b7a212c0e6aa5a6520
FranciscoAnjos <fcariaanjos@gmail.com>
2024-11-29 12:18:31 +0000

    add read_json function

---
+++ read_json.py
@@ -0,0 +1,11 @@
+import json
+
+def read_json(path):
+    try:
+        with open(path, 'r', encoding='utf-8') as f:
+            c = json.load(f)
+        print(c)
+    except Exception as e:
+        print("Ocorreu um erro!")
+    finally:
+        print("Processo concluído!")
